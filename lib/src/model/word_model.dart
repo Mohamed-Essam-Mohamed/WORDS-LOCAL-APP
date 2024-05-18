f75ece0f722a6228b0e664457ac8e60f4206099c
mohamed <esammww.com@gmail.com>
2024-05-18 14:05:39 +0300

    create word model and word typeAdapter

---
+++ lib/src/model/word_model.dart
@@ -0,0 +1,34 @@
+class WordModel {
+  final int indexAtDatabase;
+  final String text;
+  final bool isArabic;
+  final int colorCode;
+  final List<String> arabicSimilarWords;
+  final List<String> englishSimilarWords;
+  final List<String> arabicExamples;
+  final List<String> englishExamples;
+
+  const WordModel({
+    required this.indexAtDatabase,
+    required this.text,
+    required this.isArabic,
+    required this.colorCode,
+    this.arabicSimilarWords = const [],
+    this.englishSimilarWords = const [],
+    this.arabicExamples = const [],
+    this.englishExamples = const [],
+  });
+
+  WordModel decrementIndexAtDataBase() {
+    return WordModel(
+      indexAtDatabase: indexAtDatabase - 1,
+      text: text,
+      isArabic: isArabic,
+      colorCode: colorCode,
+      arabicSimilarWords: arabicSimilarWords,
+      englishSimilarWords: englishSimilarWords,
+      arabicExamples: arabicExamples,
+      englishExamples: englishExamples,
+    );
+  }
+}
