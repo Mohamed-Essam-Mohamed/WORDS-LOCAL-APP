3f95b71227f1f6b7648a1b270084884c8dfb0523
mohamed <esammww.com@gmail.com>
2024-05-18 19:52:23 +0300

    finish model WordModel

--- lib/src/model/word_model.dart
+++ lib/src/model/word_model.dart
@@ -18,15 +18,84 @@ class WordModel {
     this.arabicExamples = const [],
     this.englishExamples = const [],
   });
-
   WordModel decrementIndexAtDataBase() {
     return WordModel(
       indexAtDatabase: indexAtDatabase - 1,
       text: text,
       isArabic: isArabic,
       colorCode: colorCode,
+      arabicExamples: arabicExamples,
+      arabicSimilarWords: arabicExamples,
+      englishExamples: arabicExamples,
+      englishSimilarWords: arabicExamples,
+    );
+  }
+
+  WordModel addSimilarWord(String word, bool isArabicSimilarWord) {
+    List<String> newSimilarWords = _initializeSimilarWords(isArabicSimilarWord);
+    newSimilarWords.add(word);
+    return _getWordAfterCheckSimilarWords(isArabicSimilarWord, newSimilarWords);
+  }
+
+  WordModel deleteSimilarWord(
+      int indexAtSimilarWord, bool isArabicSimilarWord) {
+    List<String> newSimilarWords = _initializeSimilarWords(isArabicSimilarWord);
+    newSimilarWords.removeAt(indexAtSimilarWord);
+    return _getWordAfterCheckSimilarWords(isArabicSimilarWord, newSimilarWords);
+  }
+
+  WordModel addExample(String example, bool isArabicExample) {
+    List<String> newExample = _initializeExample(isArabicExample);
+    newExample.add(example);
+    return _getAfterCheckExample(isArabicExample, newExample);
+  }
+
+  WordModel deleteExaple(int indexAtExample, bool isArabicExample) {
+    List<String> newExample = _initializeExample(isArabicExample);
+    newExample.removeAt(indexAtExample);
+    return _getAfterCheckExample(isArabicExample, newExample);
+  }
+
+  WordModel _getAfterCheckExample(
+      bool isArabicExample, List<String> newExample) {
+    return WordModel(
+      indexAtDatabase: indexAtDatabase,
+      text: text,
+      isArabic: isArabic,
+      colorCode: colorCode,
+      arabicExamples: isArabicExample ? newExample : arabicExamples,
+      englishExamples: !isArabicExample ? newExample : englishExamples,
       arabicSimilarWords: arabicSimilarWords,
       englishSimilarWords: englishSimilarWords,
+    );
+  }
+
+  List<String> _initializeExample(bool isArabicSimilarWord) {
+    if (isArabicSimilarWord) {
+      return List.from(arabicExamples);
+    }
+    return List.from(englishExamples);
+  }
+
+  List<String> _initializeSimilarWords(bool isArabicSimilarWord) {
+    if (isArabicSimilarWord) {
+      return List.from(arabicSimilarWords);
+    } else {
+      return List.from(englishSimilarWords);
+    }
+  }
+
+  WordModel _getWordAfterCheckSimilarWords(
+      bool isArabicSimilarWord, List<String> newSimilarWords) {
+    return WordModel(
+      indexAtDatabase: indexAtDatabase,
+      text: text,
+      isArabic: isArabic,
+      colorCode: colorCode,
+      arabicSimilarWords:
+          isArabicSimilarWord ? newSimilarWords : arabicSimilarWords,
+      englishSimilarWords:
+          !isArabicSimilarWord ? newSimilarWords : englishSimilarWords,
       arabicExamples: arabicExamples,
       englishExamples: englishExamples,
     );
